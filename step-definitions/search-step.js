1bf6e204b07423c8a357aeb9a75a95b2cbd54122
thulasipavankumar <thulasipavankumar@gmail.com>
2024-02-05 14:46:20 +0100

    fixed failing tests

--- step-definitions/search-step.js
+++ step-definitions/search-step.js
@@ -2,18 +2,18 @@ const {HomePage} = require('../page-objects/home-page')
 const { Given, When, Then } = require('@cucumber/cucumber')
 const { test, expect } = require('@playwright/test')
 
-Given('User visit tobania website', async () => {
+Given('User visits tobania website', async () => {
   const homePage = new HomePage(global.page)
   await homePage.navigateToHomePage();
 })
 
-When('User enters the keyword in the search bar and presses enter',async (dataTable) => {
+When('User enters a keyword in the search bar and clicks the search icon',async (dataTable) => {
     const data = dataTable.rowsHash();
     const homePage = new HomePage(global.page)
     await homePage.searchKeyWordAndPressEnter(data['Query string'])
 })
 
-When('User enters the keyword in the search bar and clicks the search icon',async (dataTable) => {
+When('User enters a keyword in the search bar and presses enter',async (dataTable) => {
     const data = dataTable.rowsHash();
     const homePage = new HomePage(global.page)
     await homePage.searchKeyWordAndClick(data['Query string']);
